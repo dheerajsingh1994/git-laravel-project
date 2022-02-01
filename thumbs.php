018c4df07852799bf09ae3ca452aeb8f7f00a1b2
Erielama <dheeraj.singh1994@gmail.com>
2022-02-01 10:54:52 +0530

    thumbs

---
+++ thumbs.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Store thumb perfix with size
+ */
+return [
+
+    'user'  =>  [
+        'th_60' =>  ['postfix' => '_60x60', 'dimension' => '60x60', 'maintain' => true],
+        'th_250' =>  ['postfix' => '_250x250', 'dimension' => '250x250', 'maintain'  => true],
+    ],
+
+    'inventory'  =>  [
+        'th_60' => ['postfix' => '_60x60', 'dimension' => '60x60', 'maintain' => true],
+        'th_250' =>  ['postfix' => '_250x250', 'dimension' => '250x250', 'maintain'  => true],
+    ],
+
+    'museum'  =>  [
+        'th_60' => ['postfix' => '_60x60', 'dimension' => '60x60', 'maintain' => true],
+        'th_250' =>  ['postfix' => '_250x250', 'dimension' => '250x250', 'maintain'  => true],
+    ],
+
+];
+
+?>
